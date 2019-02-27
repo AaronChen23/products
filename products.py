bc99792deee50f18c806b32f288215e5b5bbba64
AaronChen23 <gaven2806@yahoo.com.tw>
2019-02-28 05:50:42 +0800

    read/ write/ check file

--- products.py
+++ products.py
@@ -1,4 +1,16 @@
+import os
 products = []
+
+if os.path.isfile("products.csv"):
+	print("有找到檔案嘞")
+	with open("products.csv", "r") as f:
+		for line in f:
+			if "name,price" in line :
+				continue
+			print(line.strip().split(","))
+
+else:
+	print("找不到檔案")
 
 while True:
 	name = input("請輸入你買的商品名稱: ")
@@ -12,6 +24,13 @@ print(products)
 
 for item in products:
 	print("你買的", item[0], "是", item[1], "元")
+
+with open("products.csv", "w") as f:
+	#f.write("商品" + "," + "價格" + "\n")
+	f.write("name,price\n")
+	for p in products:
+		f.write(p[0] + "," + p[1] + "\n")
+
 
 
 
